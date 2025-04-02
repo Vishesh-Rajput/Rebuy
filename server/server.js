5fc3e7e01b2dda92b3c15bc48655731fbdbc50d3
VISHESH RAJPUT <vishesh.rajput.p@gmail.com>
2025-04-02 08:49:29 +0530

    rdbms connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const db = require('./config/db'); // Import PostgreSQL connection
+
+
 const app = express();
+app.use(express.json());
+
+
+
 const port = process.env.PORT || 5000;
 
+
 app.listen(port, () => {
- console.log(`Server is now  running on port ${port}`);
+  console.log(`Server is now  running on port ${port}`);
 });
+
+module.exports = db;
