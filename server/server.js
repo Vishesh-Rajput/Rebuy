5b1b5f9baf215f87cf77367aa32c1bfb02c52abc
VISHESH RAJPUT <vishesh.rajput.p@gmail.com>
2025-04-04 11:36:07 +0530

    reverting from rdbms to mongo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require('./config/db'); // Import PostgreSQL connection
+
 
 
 const app = express();
@@ -7,11 +7,10 @@ app.use(express.json());
 
 
 
-const port = process.env.PORT || 5000;
+
 
 
 app.listen(port, () => {
   console.log(`Server is now  running on port ${port}`);
 });
 
-module.exports = db;
